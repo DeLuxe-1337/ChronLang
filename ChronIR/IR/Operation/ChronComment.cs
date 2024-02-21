21cdab2799d2792d4316637601e080786b07fe11
DeLuxe <74394136+DeLuxe-1337@users.noreply.github.com>
2024-02-21 14:08:08 -0500

    IR: Comment support

---
+++ ChronIR/IR/Operation/ChronComment.cs
@@ -0,0 +1,17 @@
+﻿using ChronIR.IR.Internal;
+
+namespace ChronIR.IR.Operation
+{
+    public class ChronComment : ChronStatement
+    {
+        private string value;
+        public ChronComment(string value)
+        {
+            this.value = value;
+        }
+        public void Write(ChronContext context)
+        {
+            context.writer.WriteLine($"//{value}");
+        }
+    }
+}
