d1507fb8cd46dbbc82d05dc5a6223620810f580b
DeLuxe <74394136+DeLuxe-1337@users.noreply.github.com>
2024-03-04 18:17:22 -0500

    ChronTemporaryVariable

---
+++ ChronIR/IR/Operation/ChronTemporaryVariable.cs
@@ -0,0 +1,28 @@
+﻿using ChronIR.IR.Internal;
+
+namespace ChronIR.IR.Operation
+{
+    public class ChronTemporaryVariable : ChronStatement, ChronExpression
+    {
+        private string name;
+        private int tempVariableNumber = ChronTypes.TEMP_VARIABLE++;
+        private ChronExpression value;
+        private string type;
+        public ChronTemporaryVariable(string name, ChronExpression value, string type = "void*")
+        {
+            this.name = $"_TEMP_{name}_{tempVariableNumber}";
+            this.value = value;
+            this.type = type;
+        }
+        public string GetName() => name;
+        public object Read(ChronContext context)
+        {
+            return name;
+        }
+        public string EmbedWrite(ChronContext context) { return $"{type} {name} = {value.Read(context)};"; }
+        public void Write(ChronContext context)
+        {
+            context.writer.WriteLine(EmbedWrite(context));
+        }
+    }
+}
