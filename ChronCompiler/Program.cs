f67b2500f06d15bac02715bb50cc1964e0ef0abc
DeLuxe <74394136+DeLuxe-1337@users.noreply.github.com>
2024-03-04 23:54:04 -0500

    Default module name changed

--- ChronCompiler/Program.cs
+++ ChronCompiler/Program.cs
@@ -6,7 +6,7 @@ class Entry
     {
         Builder.InitializeTargets();
 
-        Dictionary<string, string> parameters = new() { { "source", "main.chron" }, { "name", "MyProgram" }, { "target", "tcc" } };
+        Dictionary<string, string> parameters = new() { { "source", "main" }, { "target", "tcc" } };
         foreach (var param in args)
         {
             if (param.StartsWith("--"))
@@ -16,15 +16,19 @@ class Entry
             }
             if (param.EndsWith(".chron"))
             {
-                parameters["source"] = param;
+                parameters["source"] = param.Replace(".chron", "");
             }
         }
 
         Builder builder = new Builder();
+
+        if (!parameters.ContainsKey("name"))
+            parameters["name"] = parameters["source"];
+
         builder.Create(parameters["name"]);
         builder.Target = (parameters["target"].ToUpper());
 
-        builder.CompileChronScript(parameters["source"].Replace(".chron", ""));
+        builder.CompileChronScript(parameters["source"]);
 
         builder.Build();
     }
