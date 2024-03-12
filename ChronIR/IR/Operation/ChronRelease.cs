1ec85ad8c3bb77f160e4732575b0f6750f9a0318
DeLuxe <74394136+DeLuxe-1337@users.noreply.github.com>
2024-03-12 14:53:41 -0400

    Updated ChronRelease to use ChronTemporaryVariable

--- ChronIR/IR/Operation/ChronRelease.cs
+++ ChronIR/IR/Operation/ChronRelease.cs
@@ -5,12 +5,19 @@ namespace ChronIR.IR.Operation
     public class ChronRelease : ChronStatement, ChronExpression, ChronDeferer
     {
         private ChronExpression expression;
-        private string variableReferenceName = $"_TEMP_V{ChronTypes.TEMP_VARIABLE++}";
-        public ChronRelease(ChronExpression expr) { this.expression = expr; }
+        private ChronTemporaryVariable expressionStore;
+        private static ChronFunction MemoryRelease = new(ChronTypes.GCRelease, true);
+        public ChronRelease(ChronExpression expr) 
+        { 
+            this.expression = expr;
+            this.expressionStore = new("RELEASE_TMP", this.expression);
+        }
 
         public void Defer(ChronContext context)
         {
-            context.writer.WriteLine($"{ChronTypes.GCRelease}({variableReferenceName});");
+            ChronInvoke invoke = new(MemoryRelease);
+            invoke.AddParameter(expressionStore);
+            invoke.Write(context);
         }
         public object Read(ChronContext context)
         {
@@ -21,14 +28,16 @@ namespace ChronIR.IR.Operation
                     return expression.Read(context);
                 }
             }
-            context.writer.WriteLine($"void* {variableReferenceName} = {expression.Read(context)};");
+            expressionStore.Write(context);
             ChronDefer.Add(this);
-            return variableReferenceName;
+            return expressionStore.Read(context);
         }
 
         public void Write(ChronContext context)
         {
-            context.writer.WriteLine($"{ChronTypes.GCRelease}({expression.Read(context)});");
+            ChronInvoke invoke = new(MemoryRelease);
+            invoke.AddParameter(expression);
+            invoke.Write(context);
         }
     }
 }
