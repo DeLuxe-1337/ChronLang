6ec260b4f59317908e45e07f9d374e3db9f3bad3
DeLuxe <74394136+DeLuxe-1337@users.noreply.github.com>
2024-04-25 20:55:24 -0400

    Setup bootstrap self-compilation

---
+++ src/compile_bootstrapped_compiler.bat
@@ -0,0 +1,4 @@
+del bootstrap.exe
+tcc c_backend/memory.c c_backend/object.c c_backend/standard.c bootstrap.chron.c -o bootstrap.exe
+bootstrap.exe
+pause
---
+++ src/compile_test.bat
@@ -0,0 +1,4 @@
+del test.exe
+tcc c_backend/memory.c c_backend/object.c c_backend/standard.c test.chron.c -o test.exe 
+test.exe
+pause
